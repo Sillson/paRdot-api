05d6dd03c455e331d688187b17b8cd099491ac84
Stuart Illson <sillson88@gmail.com>
2016-08-08 12:53:39 -0700

    retries api call for authentication. will need to build/split params

--- api.R
+++ api.R
@@ -1,5 +1,6 @@
 require(httr)
 require(xml2)
+require(jsonlite)
 
 authenticate <- function() {
   # body params must be set in list. Add .env get that will fetch these items
@@ -11,18 +12,30 @@ authenticate <- function() {
   fetch_api_call <- POST("https://pi.pardot.com/api/login/version/3", body= auth_body)
 
   # returns xml node with <api_key>
-  api_key <- xml_text(content(fetch_api_call))
+  api_key <<- xml_text(content(fetch_api_call))
 }
 
 pardot_client <- function(params) {
   
   if (is.null(api_key)) {
-    authenticate
+    authenticate()
   }
 
-  api_call(params)
+  request_body <- build_url(params)
+  api_call(request_body)
 }
 
-api_call <- function(params) {
-  GET("https://pi.pardot.com/api/<object>/version/3/do/query/<identifier_field>/<identifier>?api_key=<your_api_key>&user_key=<your_user_key>&<parameters_for_request>")
+api_call <- function(request_body) {
+  GET(request_body)
+}
+
+build_url <- function(params) {
+  api_object <- 
+  api_operator <- 
+  api_identifier_field <- 
+  api_identifier <- 
+  api_request_params <- 
+  api_format <- 
+
+  cat("https://pi.pardot.com/api/",api_object,"/version/3/do/"api_operator"/"api_identifier_field"/"api_identifier"?api_key=",api_key,"&user_key=",Sys.getenv("PARDOT_USER_KEY"),"&",api_request_params,"&format=",api_format)
 }
